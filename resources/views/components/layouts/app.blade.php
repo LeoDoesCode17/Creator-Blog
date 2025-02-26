8049170dc0d415b56775c44f201df3b3d2981792
Leo <leonardonifinluri@gmail.com>
2025-02-26 22:06:29 +0800

    modify navbar

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,9 +5,22 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>{{ $title ?? 'Page Title' }}</title>
+
+        @livewireStyles
+        @vite(['resources/js/app.js', 'resources/css/app.css'])
+
     </head>
     <body>
-        <div>Teknik</div>
-        {{ $slot }}
+        <div class="min-h-full">
+            
+            <livewire:nav-bar />
+            <livewire:header title="Home"/>
+            <main>
+                <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                    {{ $slot }}
+                </div>
+            </main>
+        </div> 
+        @livewireScriptConfig
     </body>
 </html>
