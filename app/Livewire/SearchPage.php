c391481d7140392a478a5ed1ff12dfed9673d589
Leo <leonardonifinluri@gmail.com>
2025-02-26 22:14:30 +0800

    feat(page_title): add page title for both page

--- app/Livewire/SearchPage.php
+++ app/Livewire/SearchPage.php
@@ -2,10 +2,12 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class SearchPage extends Component
 {
+    #[Title('Search')]
     public function render()
     {
         return view('livewire.search-page');
