0db22018727072491c26c5288ab0a48a274067be
Leo <leonardonifinluri@gmail.com>
2025-02-26 22:20:55 +0800

    feat(routes): add profile routes

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 use App\Livewire\HomePage;
 use App\Livewire\Login;
+use App\Livewire\ProfilePage;
 use App\Livewire\SearchPage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomePage::class)->name('home');
 Route::get('/search', SearchPage::class)->name('search');
+Route::get('/profile', ProfilePage::class)->name('profile');
