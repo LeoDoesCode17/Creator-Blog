29c38c81ccfe0d7da3263f1ac9b956136c3b59ef
Leo <leonardonifinluri@gmail.com>
2025-02-26 22:16:45 +0800

    refactor(header): change header to be the same as page title

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -14,7 +14,7 @@
         <div class="min-h-full">
             
             <livewire:nav-bar />
-            <livewire:header title="Home"/>
+            <livewire:header :title="$title"/>
             <main>
                 <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                     {{ $slot }}
