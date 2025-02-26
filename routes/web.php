ef5c5e85b40d0716ebc88570fc03cdf7d43ceb6b
Leo <leonardonifinluri@gmail.com>
2025-02-26 22:25:13 +0800

    feat(routes): add setting rotue

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,10 @@ use App\Livewire\HomePage;
 use App\Livewire\Login;
 use App\Livewire\ProfilePage;
 use App\Livewire\SearchPage;
+use App\Livewire\SettingsPage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomePage::class)->name('home');
 Route::get('/search', SearchPage::class)->name('search');
 Route::get('/profile', ProfilePage::class)->name('profile');
+Route::get('/settings', SettingsPage::class)->name('settings');
