6f9046ad0020a0fe40e7dc8022e97fc7ae0e30e8
Leo <leonardonifinluri@gmail.com>
2025-02-26 22:23:20 +0800

    feat(navbar): add routes for your profile item in menu

--- app/Livewire/ProfilePage.php
+++ app/Livewire/ProfilePage.php
@@ -2,10 +2,12 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class ProfilePage extends Component
 {
+    #[Title('Profile')]
     public function render()
     {
         return view('livewire.profile-page');
