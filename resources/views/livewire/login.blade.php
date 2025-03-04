8e25716dede8e3c30dfb1493b07f22303844ff87
Leo <leonardonifinluri@gmail.com>
2025-03-04 21:32:46 +0800

    feat: add navigation to register

--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -32,7 +32,7 @@
 
     <p class="mt-10 text-center text-sm/6 text-gray-500">
       Not register yet?
-      <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Create your account</a>
+      <a href="{{ route('register') }}" class="font-semibold text-indigo-600 hover:text-indigo-500">Create your account</a>
     </p>
   </div>
 </div>
