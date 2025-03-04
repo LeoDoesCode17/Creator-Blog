b24916ed58bb84db54b6a05437ef307117d916ca
Leo <leonardonifinluri@gmail.com>
2025-03-04 20:51:30 +0800

    feat: add temporary routes for login page

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Livewire\SettingsPage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomePage::class)->name('home');
+Route::get('/login', Login::class)->name('login');
 Route::get('/search', SearchPage::class)->name('search');
 Route::get('/profile', ProfilePage::class)->name('profile');
 Route::get('/settings', SettingsPage::class)->name('settings');
