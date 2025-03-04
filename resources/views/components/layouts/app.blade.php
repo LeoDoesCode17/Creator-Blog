ab8b73082ea7b8c83262bfc179169a5678642509
Leo <leonardonifinluri@gmail.com>
2025-03-04 20:52:04 +0800

    refactor: modify the title to null coalescing

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -14,7 +14,7 @@
         <div class="min-h-full">
             
             <livewire:nav-bar />
-            <livewire:header :title="$title"/>
+            <livewire:header :title="$title ?? 'Page Title'"/>
             <main>
                 <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                     {{ $slot }}
