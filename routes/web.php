bbf343f92fc4bfe77c995e58afeea9439a442ae1
Leo <leonardonifinluri@gmail.com>
2025-03-05 12:19:11 +0800

    feat: implement middleware for auth

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,13 @@ use App\Livewire\SearchPage;
 use App\Livewire\SettingsPage;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', HomePage::class)->name('home');
-Route::get('/login', LoginPage::class)->name('login');
-Route::get('/register', RegisterPage::class)->name('register');
-Route::get('/search', SearchPage::class)->name('search');
-Route::get('/profile', ProfilePage::class)->name('profile');
-Route::get('/settings', SettingsPage::class)->name('settings');
+Route::middleware('auth')->group(function (){
+    Route::get('/', HomePage::class)->name('home');
+    Route::get('/search', SearchPage::class)->name('search');
+    Route::get('/profile', ProfilePage::class)->name('profile');
+    Route::get('/settings', SettingsPage::class)->name('settings');
+});
+Route::middleware('guest')->group(function (){
+    Route::get('/login', LoginPage::class)->name('login');
+    Route::get('/register', RegisterPage::class)->name('register');
+});
