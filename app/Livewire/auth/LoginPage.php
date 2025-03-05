28b39829bd165304a803189127cb4006dcf411bd
Leo <leonardonifinluri@gmail.com>
2025-03-05 12:42:34 +0800

    feat: add login function

--- app/Livewire/auth/LoginPage.php
+++ app/Livewire/auth/LoginPage.php
@@ -8,6 +8,18 @@ use Livewire\Component;
 #[Title('Login')]
 class LoginPage extends Component
 {
+    //identifier means email or username
+    public $identifier;
+    public $password;
+
+    public function login(){
+        $data = [
+            'identifier' => $this->identifier,
+            'password' => $this->password,
+        ];
+        dd($data);
+    }
+
     public function render()
     {
         return view('livewire.auth.login-page');
