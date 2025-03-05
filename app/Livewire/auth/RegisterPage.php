f8df358849719ce2ef0880c32dcd6d254e45a6be
Leo <leonardonifinluri@gmail.com>
2025-03-05 14:59:34 +0800

    feat: add data needed to register

--- app/Livewire/auth/RegisterPage.php
+++ app/Livewire/auth/RegisterPage.php
@@ -8,6 +8,13 @@ use Livewire\Component;
 #[Title('Register')]
 class RegisterPage extends Component
 {
+
+    public $name;
+    public $username;
+    public $email;
+    public $password;
+    public $passwordConfirmation;
+
     public function render()
     {
         return view('livewire.auth.register-page');
