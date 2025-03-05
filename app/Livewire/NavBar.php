3e8b112adfe6d845316c4004416e9890022e41cc
Leo <leonardonifinluri@gmail.com>
2025-03-05 14:46:17 +0800

    refactor: move logout to web and remove from navbar

--- app/Livewire/NavBar.php
+++ app/Livewire/NavBar.php
@@ -8,20 +8,6 @@ use Livewire\Component;
 class NavBar extends Component
 {
 
-    public function logout(){
-        //Remove the user from laravel's auth system
-        Auth::logout();
-
-        //Destroy the current session because the old session id can still be used (session fixation attack risk)
-        session()->invalidate();
-
-        //Generate a new CSRF token because the old token can still be used (CSRF attack risk)
-        session()->regenerateToken();
-
-        //Redirect to loginn page
-        return redirect()->route('login');
-    }
-
     public function render()
     {
         return view('livewire.nav-bar');
