35191ddddc2311e536ec34f77570d5b21d522ec6
Leo <leonardonifinluri@gmail.com>
2025-03-05 13:51:40 +0800

    feat: complete login function without validate

--- app/Livewire/auth/LoginPage.php
+++ app/Livewire/auth/LoginPage.php
@@ -2,8 +2,12 @@
 
 namespace App\Livewire\Auth;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 use Livewire\Attributes\Title;
 use Livewire\Component;
+
+use function PHPUnit\Framework\throwException;
 
 #[Title('Login')]
 class LoginPage extends Component
@@ -12,12 +16,30 @@ class LoginPage extends Component
     public $identifier;
     public $password;
 
+    protected $rules = [
+        'identifier' => 'required',
+        'password' => 'required|min:8',
+    ];
+
     public function login(){
-        $data = [
-            'identifier' => $this->identifier,
+
+        //determine if the identifier is an email or username
+        $fieldType = filter_var($this->identifier, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+
+        $this->validate();
+
+        //atempt to login
+        if(Auth::attempt([
+            $fieldType => $this->identifier,
             'password' => $this->password,
-        ];
-        dd($data);
+        ])){
+            return redirect()->route('home');
+        }
+
+        throw ValidationException::withMessages([
+            'auth' => 'Invalid credentials'
+        ]);
+
     }
 
     public function render()
