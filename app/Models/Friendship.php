c4febb7d2946bc512268143e58c0bac508db4c15
Leo <leonardonifinluri@gmail.com>
2025-03-08 21:41:09 +0800

    feat: define friendship model

--- app/Models/Friendship.php
+++ app/Models/Friendship.php
@@ -9,4 +9,20 @@ class Friendship extends Model
 {
     /** @use HasFactory<\Database\Factories\FriendshipFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'friend_id',
+        'status'
+    ];
+
+    //to get the sender of a friendship
+    public function sender(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    //to get the receiver of a friendship
+    public function receiver(){
+        return $this->belongsTo(User::class, 'friend_id');
+    }
 }
