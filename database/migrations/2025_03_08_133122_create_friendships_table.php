24472faa4b83705e83e19d40b521eedbb42c79de
Leo <leonardonifinluri@gmail.com>
2025-03-12 11:32:28 +0800

    refactor(friendship migration): modify to sender-receiver

--- database/migrations/2025_03_08_133122_create_friendships_table.php
+++ database/migrations/2025_03_08_133122_create_friendships_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('friendships', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); //this is sender
-            $table->foreignId('friend_id')->constrained('users')->onDelete('cascade'); //this is receiver
+            $table->foreignId('sender_id')->constrained('users')->onDelete('cascade'); //this is sender
+            $table->foreignId('receiver_id')->constrained('users')->onDelete('cascade'); //this is receiver
             $table->enum('status', ['pending', 'accepted', 'declined', 'blocked'])->default('pending'); //this is the status of friendship
             $table->timestamps();
         });
