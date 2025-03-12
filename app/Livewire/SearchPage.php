0e855d883e38189620b76f0efbe2c0e3e81a0cab
Leo <leonardonifinluri@gmail.com>
2025-03-12 12:45:25 +0800

    feat(search page): get all users except authed user

--- app/Livewire/SearchPage.php
+++ app/Livewire/SearchPage.php
@@ -2,12 +2,19 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class SearchPage extends Component
 {
     #[Title('Search')]
+
+    public $users;
+
+    public function mount(){
+        $this->users = User::all()->where('id', '!=', auth()->id());
+    }
     public function render()
     {
         return view('livewire.search-page');
