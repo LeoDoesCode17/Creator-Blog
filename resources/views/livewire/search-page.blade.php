02b21009d11f5b5ace30ec587d7ce5d0e5a14bed
Leo <leonardonifinluri@gmail.com>
2025-03-12 12:45:55 +0800

    feat(search page): display all users in a table

--- resources/views/livewire/search-page.blade.php
+++ resources/views/livewire/search-page.blade.php
@@ -1,3 +1,46 @@
 <div>
     This is search page
+    <div
+        class="relative flex flex-col w-full h-full overflow-scroll text-gray-700 bg-white shadow-md rounded-xl bg-clip-border mt-3">
+        <table class="w-full text-left table-auto min-w-max">
+            <thead>
+                <tr>
+                    <th class="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                        <p class="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
+                            Name
+                        </p>
+                    </th>
+                    <th class="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                        <p class="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
+                            Username
+                        </p>
+                    </th>
+                    <th class="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                        <p class="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">Friend Status</p>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($users as $user)
+                    <tr>
+                        <td class="p-4 border-b border-blue-gray-50">
+                            <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                                {{ $user->name }}
+                            </p>
+                        </td>
+                        <td class="p-4 border-b border-blue-gray-50">
+                            <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                                {{ $user->username }}
+                            </p>
+                        </td>
+                        <td class="p-4 border-b border-blue-gray-50">
+                            <a href="#" class="block font-sans text-sm antialiased font-medium leading-normal text-blue-gray-900 hover:underline">
+                                Add Friend
+                            </a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
