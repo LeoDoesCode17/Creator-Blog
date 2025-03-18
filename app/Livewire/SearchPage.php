14ea850d51089ce177e5af5df1b6f552d5f81be4
Leo <leonardonifinluri@gmail.com>
2025-03-18 12:04:54 +0800

    feat: create updatedSearch and dispatch event

--- app/Livewire/SearchPage.php
+++ app/Livewire/SearchPage.php
@@ -11,12 +11,23 @@ class SearchPage extends Component
 {
     #[Title('Search')]
 
-    public $users;
+    public $search = '';
 
-    public function mount(){
-        $this->users = User::all();
+    /**
+     * Updates the search results based on the user's input.
+     *
+     * This method is triggered whenever the `search` property is updated.
+     * It performs a search query on the `User` model using the current value
+     * of the `search` property and retrieves the matching results. The results
+     * are then dispatched as an event named `searchUpdated`.
+     *
+     * @return void
+     */
+    public function updatedSearch(){
+        $users = User::search($this->search)->get();
+        $this->dispatch('searchUpdated', $users);
     }
-    
+
     public function render()
     {
         return view('livewire.search-page');
