b91ff55c4a62abfc6be10945372aa39b5aaeae54
Leo <leonardonifinluri@gmail.com>
2025-03-18 13:46:51 +0800

    feat: add exceptional condition for authed users profile page

--- resources/views/livewire/users-list.blade.php
+++ resources/views/livewire/users-list.blade.php
@@ -27,7 +27,7 @@
                 <td class="p-4 border-b border-blue-gray-50">
                     <a class="block font-sans text-sm antialiased font-normal leading-normal 
          text-blue-gray-900 hover:text-blue-500"
-                        href="{{ route('user.show', $user['username']) }}">
+                        href="{{ $user['id'] == auth()->id() ? route('profile') : route('user.show', $user['username']) }}">
                         {{ $user['username'] }}
                     </a>
 
