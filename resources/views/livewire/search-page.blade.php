241d1e71f4d607f8ab23ee0ba6f7df2a17bdb26d
Leo <leonardonifinluri@gmail.com>
2025-03-18 12:06:03 +0800

    feat: call data in UI

--- resources/views/livewire/search-page.blade.php
+++ resources/views/livewire/search-page.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div>
-        <input type="text" name="search" id="search" placeholder="Search User" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 border border-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" wire:model.live.debounce.500ms="search">
+        <input type="text" name="search" id="search" placeholder="Search User" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 border border-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" wire:model.live.debounce.300ms="search">
     </div>
     @livewire('users-list')
 </div>
--- resources/views/livewire/users-list.blade.php
+++ resources/views/livewire/users-list.blade.php
@@ -1,41 +1,39 @@
 <div
     class="relative flex flex-col w-full h-full overflow-scroll text-gray-700 bg-white shadow-md rounded-xl bg-clip-border mt-3">
     @if ($users)
-        <table class="w-full text-left table-auto min-w-max">
-            <thead>
-                <tr>
-                    <th class="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
-                        <p class="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
-                            Name
-                        </p>
-                    </th>
-                    <th class="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
-                        <p class="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
-                            Username
-                        </p>
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($users as $user)
-                <tr>
-                    <td class="p-4 border-b border-blue-gray-50">
-                        <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-                            {{ $user->name }}
-                        </p>
-                    </td>
-                    <td class="p-4 border-b border-blue-gray-50">
-                        <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-                            {{ $user->username }}
-                        </p>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+    <table class="w-full text-left table-auto min-w-max">
+        <thead>
+            <tr>
+                <th class="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                    <p class="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
+                        Name
+                    </p>
+                </th>
+                <th class="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                    <p class="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
+                        Username
+                    </p>
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($users as $user)
+            <tr>
+                <td class="p-4 border-b border-blue-gray-50">
+                    <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                        {{ $user['name'] }}
+                    </p>
+                </td>
+                <td class="p-4 border-b border-blue-gray-50">
+                    <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                        {{ $user['username'] }}
+                    </p>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
     @else
-        <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-            {{ $search }} not found
-        </p>
+    User Not Found
     @endif
 </div>
