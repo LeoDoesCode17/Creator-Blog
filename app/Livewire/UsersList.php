ea75ac493e5fe0ede7121cf7cfbb28e0fdf0ada0
Leo <leonardonifinluri@gmail.com>
2025-03-18 12:05:44 +0800

    feat: create updateUsers when listener detect searchUpdated

--- app/Livewire/UsersList.php
+++ app/Livewire/UsersList.php
@@ -2,14 +2,31 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
 
 class UsersList extends Component
 {
 
-    public $users;
+    public $users = [];
 
-    public $search;
+    public function mount(){
+        $this->users = User::all()->toArray();
+    }
+
+    /**
+     * The event listeners for the Livewire component.
+     *
+     * @var array
+     * 
+     * This property listens for the 'searchUpdated' event and triggers the 
+     * 'updateUsers' method when the event is fired.
+     */
+    protected $listeners = ['searchUpdated' => 'updateUsers',];
+
+    public function updateUsers($users){
+        $this->users = $users;
+    }
 
     public function render()
     {
