ba1b911abde95c8808c3ab5ab6d5aafb544f1dee
Leo <leonardonifinluri@gmail.com>
2025-04-03 18:23:47 +0800

    feat: create new livewire component, follow button

---
+++ resources/views/livewire/components/follow-button.blade.php
@@ -0,0 +1,5 @@
+<button
+    class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 bg-gradient-to-tr from-gray-900 to-gray-800 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] rounded-full"
+    type="button">
+    ADD FRIEND
+</button>
