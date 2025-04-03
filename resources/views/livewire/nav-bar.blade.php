ea4d59a87373f2f57172bb5b740c038fd6b19638
Leo <leonardonifinluri@gmail.com>
2025-04-03 12:00:10 +0800

    feat: add default avatar in nav-bar from authed user

--- resources/views/livewire/nav-bar.blade.php
+++ resources/views/livewire/nav-bar.blade.php
@@ -28,7 +28,7 @@
                             <button @click="displayed = !displayed" type="button" class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                                 <span class="absolute -inset-1.5"></span>
                                 <span class="sr-only">Open user menu</span>
-                                <img class="size-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                                <img class="size-8 rounded-full" src="{{ asset($user->avatar) }}" alt="">
                             </button>
                         </div>
 
@@ -83,7 +83,7 @@
         <div class="border-t border-gray-700 pt-4 pb-3">
             <div class="flex items-center px-5">
                 <div class="shrink-0">
-                    <img class="size-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                    <img class="size-10 rounded-full" src="{{ asset($user->avatar) }}" alt="">
                 </div>
                 <div class="ml-3">
                     <div class="text-base/5 font-medium text-white">{{ $user->name }}</div>
