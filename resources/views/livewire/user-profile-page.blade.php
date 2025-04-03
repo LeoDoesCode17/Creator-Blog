3127dfec32fdfc336b632c2ddd513a71df3310cb
Leo <leonardonifinluri@gmail.com>
2025-04-03 18:25:24 +0800

    feat: display friendship status

--- resources/views/livewire/user-profile-page.blade.php
+++ resources/views/livewire/user-profile-page.blade.php
@@ -1,3 +1,23 @@
 <div>
+    @php
+        use App\Enums\FriendshipStatus;
+    @endphp
+
     {{ $user->name }}
+    @if (!$friendship)
+        <p>Send friend request to {{ $user->name }}</p>
+        @livewire('components.follow-button')
+    @else
+        @if ($friendship->status == FriendshipStatus::PENDING->value)
+            <p>Friendship status: pending</p>
+        @elseif ($friendship->status == FriendshipStatus::ACCEPTED->value)
+            <p>You and {{ $user->name }} are friends</p>
+        @elseif ($friendship->status == FriendshipStatus::DECLINED->value)
+            <p>Send friend request to {{ $user->name }}</p>
+            @livewire('components.follow-button')
+        @else
+            <p></p>
+        @endif
+    @endif
+
 </div>
