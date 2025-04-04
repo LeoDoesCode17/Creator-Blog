761d7e65285736bb243dfe6491ac98440bddbf46
Leo <leonardonifinluri@gmail.com>
2025-04-04 21:52:26 +0800

    feat: complete functionality for send friend request

--- resources/views/livewire/components/add-friend-button.blade.php
+++ resources/views/livewire/components/add-friend-button.blade.php
@@ -1,5 +1,11 @@
-<button
-    class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 bg-gradient-to-tr from-gray-900 to-gray-800 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] rounded-full"
-    type="button">
-    ADD FRIEND
-</button>
+<div>
+    <button
+        class="px-4 py-2 rounded-md transition text-white
+               bg-gray-600 hover:bg-gray-800
+               disabled:bg-gray-400 disabled:cursor-not-allowed"
+        wire:click="addFriend"
+        wire:loading.attr="disabled">
+        Add Friend
+    </button>
+    <span wire:loading>Sending...</span>
+</div>
