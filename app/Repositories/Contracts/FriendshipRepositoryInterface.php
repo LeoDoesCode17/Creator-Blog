daa01d7ee8099f5c76d0ed969d4075c4f6f36b7d
Leo <leonardonifinluri@gmail.com>
2025-04-06 19:40:45 +0800

    feat: define function to accept, decline, and block friendship request

--- app/Repositories/Contracts/FriendshipRepositoryInterface.php
+++ app/Repositories/Contracts/FriendshipRepositoryInterface.php
@@ -8,4 +8,7 @@ interface FriendshipRepositoryInterface
 {
     public function between(User $authedUser, User $visitedUser);
     public function upsertFriendshipRequest(User $authedUser, User $receiverUser);
+    public function acceptFriendshipRequest(User $authedUser, User $targetUser);
+    public function declineFriendshipRequest(User $authedUser, User $targetUser);
+    public function blockFriendshipRequest(User $authedUser, User $targetUser);
 }
