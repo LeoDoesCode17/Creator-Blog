ca5994e9da7906929ffe602432395cace926e273
Leo <leonardonifinluri@gmail.com>
2025-04-06 19:29:34 +0800

    refactor: remove unused functions

--- app/Models/Friendship.php
+++ app/Models/Friendship.php
@@ -51,43 +51,4 @@ class Friendship extends Model
             $query->where('sender_id', $receiver_id)->where('receiver_id', $sender_id);
         })->first();
     }
-
-    public static function createOrUpdateFriendshipRequest(User $authedUser, User $receiverUser)
-    {
-        $friendshipRequest = $authedUser->getFriendshipReceiverStatus($receiverUser->id);
-
-        //check if the friendshipRequest already exists
-        if ($friendshipRequest) {
-            if ($friendshipRequest->status == FriendshipStatus::DECLINED->value) {
-                $friendshipRequest->update([
-                    'status' => FriendshipStatus::PENDING->value,
-                    'updated_at' => now(),
-                ]);
-            }
-            return $friendshipRequest; // always return object (even if not updated)
-        }
-
-        return self::create([
-            'sender_id' => $authedUser->id,
-            'receiver_id' => $receiverUser->id,
-            'status' => FriendshipStatus::PENDING->value,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
-    }
-
-    //function to get information about friendship request between authed user and visitedUser and information about whether the authed user is the sender or receiver of the friendship request
-    public static function between(User $authedUser, User $visitedUser)
-    {
-        $authedAsSender = $authedUser->getFriendshipReceiverStatus($visitedUser->id);
-        $authedAsReceiver = $visitedUser->getFriendshipReceiverStatus($authedUser->id);
-
-        //data means the frienship request model
-        return (object)[
-            // if authedAsSender not null then store it in data else store authedAsReceiver in data
-            'data' => $authedAsSender ?? $authedAsReceiver,
-            // if authedAsSender not null then authed user is sender if authedAsReceiver not null authed user is receiver else null
-            'isSender' => $authedAsSender ? true : ($authedAsReceiver ? false : null),
-        ];
-    }
 }
