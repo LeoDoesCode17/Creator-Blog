9c8a6863e35dab2249c68d8721db88f857f7a233
Leo <leonardonifinluri@gmail.com>
2025-05-03 22:11:23 +0800

    refactor: change function name to more general

--- app/Repositories/Contracts/PostRepositoryInterface.php
+++ app/Repositories/Contracts/PostRepositoryInterface.php
@@ -4,10 +4,10 @@ namespace App\Repositories\Contracts;
 
 interface PostRepositoryInterface
 {
-    public function getAllPosts($perPage = 6, $with = []);
-    public function getPostById($id, $with = []);
+    public function getAllPosts($perPage, $with);
+    public function getPostById($id, $with);
     public function createPost(array $data);
     public function updatePost($id, array $data);
     public function deletePost($id);
-    public function getFriendPosts($userId, $perPage = 6, $with = []);
+    public function getPostsByUser($userId, $perPage, $with);
 }
