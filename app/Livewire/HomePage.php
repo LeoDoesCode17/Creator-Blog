1747c216615633bde084e5b5f2c2c6af253514a8
Leo <leonardonifinluri@gmail.com>
2025-05-03 12:04:51 +0800

    feat: get all posts and pass it to home page

--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\Post;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
@@ -10,6 +11,7 @@ class HomePage extends Component
     #[Title('Home')]
     public function render()
     {
-        return view('livewire.home-page');
+        $posts = Post::all();
+        return view('livewire.home-page', ['posts' => $posts]);
     }
 }
