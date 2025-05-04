0306fedea14341b1d09a5c02076a344bb521e519
Leo <leonardonifinluri@gmail.com>
2025-05-04 19:46:02 +0800

    feat: add functions to react over a friendship request

--- app/Services/FriendshipService.php
+++ app/Services/FriendshipService.php
@@ -20,4 +20,28 @@ class FriendshipService
         //this function to get friendship request betweem two users
         return $this->friendshipRespository->getFriendshipBetween($user1, $user2);
     }
+
+    public function addAsFriend(User $user1, User $user2)
+    {
+        //this function add new friend where the sender is $user1 and the receiver is $user2
+        return $this->friendshipRespository->upsertFriendshipRequest($user1, $user2);
+    }
+
+    public function acceptFriendRequest(User $user1, User $user2)
+    {
+        //this function accept the friend request where the sender is $user1 and the receiver is $user2
+        return $this->friendshipRespository->acceptFriendshipRequest($user2, $user1);
+    }
+
+    public function declineFriendRequest(User $user1, User $user2)
+    {
+        //this function decline the friend request where the sender is $user1 and the receiver is $user2
+        return $this->friendshipRespository->declineFriendshipRequest($user2, $user1);
+    }
+
+    public function blockFriendRequest(User $user1, User $user2)
+    {
+        //this function block the friend request where the sender is $user1 and the receiver is $user2
+        return $this->friendshipRespository->blockFriendshipRequest($user2, $user1);
+    }
 }
